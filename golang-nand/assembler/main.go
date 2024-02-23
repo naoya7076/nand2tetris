cbd3355b413f6e5757b126e382f410c43b488981
naoya7076 <naoya7076@gmail.com>
2024-02-24 00:33:00 +0900

    Assemblerとして切り出す

--- golang-nand/assembler/main.go
+++ golang-nand/assembler/main.go
@@ -18,9 +18,21 @@ func main() {
 		fmt.Println("File not found")
 	}
 	defer file.Close()
+	// 出力ファイルを作成
+	outputFileName := fileName[:len(fileName)-3] + "sample.hack"
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		fmt.Println("File not found")
+	}
+	defer outputFile.Close()
+
+	Assemble(file, outputFile)
+}
+
+func Assemble(in *os.File, out *os.File) {
 	// 入力ファイルのテキストからコメントアウトと空白行を削除
 	trimmedSrc := ""
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(in)
 	for scanner.Scan() {
 		text := scanner.Text()
 		// ~コメントアウトと空白行をfileから削除
@@ -29,27 +41,20 @@ func main() {
 		}
 		trimmedSrc += strings.ReplaceAll(text, " ", "") + "\n"
 	}
-	// 出力ファイルを作成
-	outputFileName := fileName[:len(fileName)-3] + "sample.hack"
-	outputFile, err := os.Create(outputFileName)
-	if err != nil {
-		fmt.Println("File not found")
-	}
-	defer outputFile.Close()
 	// 入力ファイルをパース
 	// パースした結果を出力ファイルに書き込む
 	p := parser.New(trimmedSrc)
 	for p.HasMoreCommands() {
 		switch p.CommandType() {
 		case "A_COMMAND":
-			outputFile.WriteString(p.Symbol() + "\n")
+			out.WriteString(p.Symbol() + "\n")
 		case "L_COMMAND":
-			outputFile.WriteString(p.Symbol() + "\n")
+			out.WriteString(p.Symbol() + "\n")
 		case "C_COMMAND":
 			dest := code.Dest(p.Dest())
 			comp := code.Comp(p.Comp())
 			jump := code.Jump(p.Jump())
-			outputFile.WriteString("111" + comp + dest + jump + "\n")
+			out.WriteString("111" + comp + dest + jump + "\n")
 		default:
 			fmt.Println("Command type not found")
 		}
