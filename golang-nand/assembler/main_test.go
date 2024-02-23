fb3ed650faf2197b10eb93a14382416087df2d57
naoya7076 <naoya7076@gmail.com>
2024-02-24 01:06:49 +0900

    データドリブンテストをしやすいように修正

--- golang-nand/assembler/main_test.go
+++ golang-nand/assembler/main_test.go
@@ -3,38 +3,41 @@ package main
 import (
 	"bytes"
 	"os"
-	"path/filepath"
 	"testing"
 )
 
 // MaxL.asmを変換してMaxL.hackと比較する
 func TestAssemble(t *testing.T) {
-	// テストデータのパスを取得
-	inPath := filepath.Join("./asm/max/MaxL.asm")
-	inFile, err := os.Open(inPath)
-	if err != nil {
-		t.Fatal(err)
+	cases := []struct {
+		inPath   string
+		outPath  string
+		basePath string
+	}{
+		{"./asm/max/MaxL.asm", "./asm/max/MaxL.hack", "./testdata/MaxL.hack"},
 	}
-	defer inFile.Close()
-	// 出力ファイルのパスを取得
-	outPath := filepath.Join("./asm/max/dist/MaxL.hack")
-	outFile, err := os.Create(outPath)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer outFile.Close()
-	// Assemble関数を実行
-	Assemble(inFile, outFile)
-	comp, err := os.ReadFile("./asm/max/MaxL.hack")
-	if err != nil {
-		t.Fatal(err)
-	}
-	out, err := os.ReadFile("./asm/max/dist/MaxL.hack")
-	if err != nil {
-		t.Fatal(err)
-	}
-	// 出力ファイルと期待するファイルを比較
-	if !bytes.Equal(comp, out) {
-		t.Fatalf("expected %s, but got %s", comp, out)
+	for _, tt := range cases {
+		inFile, err := os.Open(tt.inPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer inFile.Close()
+		outFile, err := os.Create(tt.outPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer outFile.Close()
+		Assemble(inFile, outFile)
+		// 出力ファイルと期待するファイルを比較
+		base, err := os.ReadFile(tt.basePath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		out, err := os.ReadFile(tt.outPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(base, out) {
+			t.Fatalf("expected %s, but got %s", base, out)
+		}
 	}
 }
