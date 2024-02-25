e538a9d28095201c1c2276634254e737f86400eb
naoya7076 <naoya7076@gmail.com>
2024-02-25 19:59:42 +0900

    1回目のsymbol tableを作成する

--- golang-nand/assembler/main.go
+++ golang-nand/assembler/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"assembler/code"
 	"assembler/parser"
+	"assembler/symboltable"
 	"bufio"
 	"fmt"
 	"os"
@@ -35,22 +36,45 @@ func Assemble(in string, out *os.File) {
 	// 入力ファイルをパース
 	// パースした結果を出力ファイルに書き込む
 	p := parser.New(in)
+	st := firstPass(p)
+	p2 := parser.New(in)
+	for p2.HasMoreCommands() {
+		switch p2.CommandType() {
+		case "A_COMMAND":
+			out.WriteString(p2.Symbol() + "\n")
+		case "L_COMMAND":
+			out.WriteString(p2.Symbol() + "\n")
+		case "C_COMMAND":
+			dest := code.Dest(p2.Dest())
+			comp := code.Comp(p2.Comp())
+			jump := code.Jump(p2.Jump())
+			out.WriteString("111" + comp + dest + jump + "\n")
+		default:
+			fmt.Println("Command type not found")
+		}
+		p2.Advance()
+	}
+	fmt.Printf("SymbolTable: %v\n", st)
+}
+
+func firstPass(p *parser.Parser) *symboltable.SymbolTable {
+	st := symboltable.New()
+	// 1回目のパスでラベルを探し、シンボルテーブルに追加する
 	for p.HasMoreCommands() {
 		switch p.CommandType() {
 		case "A_COMMAND":
-			out.WriteString(p.Symbol() + "\n")
+			st.IncRomAddress()
 		case "L_COMMAND":
-			out.WriteString(p.Symbol() + "\n")
+			symbol := p.Symbol()
+			st.AddEntry(symbol, st.GetRomAddress()+1)
 		case "C_COMMAND":
-			dest := code.Dest(p.Dest())
-			comp := code.Comp(p.Comp())
-			jump := code.Jump(p.Jump())
-			out.WriteString("111" + comp + dest + jump + "\n")
+			st.IncRomAddress()
 		default:
 			fmt.Println("Command type not found")
 		}
 		p.Advance()
 	}
+	return st
 }
 
 func trimFileText(input *os.File) string {
