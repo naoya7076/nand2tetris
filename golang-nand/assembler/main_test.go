ffa318608bdf408702df6bbd1d728842e6124912
naoya7076 <naoya7076@gmail.com>
2024-02-26 16:20:34 +0900

    Max.asmを変換するテストを追加

--- golang-nand/assembler/main_test.go
+++ golang-nand/assembler/main_test.go
@@ -14,6 +14,7 @@ func TestAssemble(t *testing.T) {
 		basePath string
 	}{
 		{"./asm/max/MaxL.asm", "./asm/max/MaxL.hack", "./testdata/MaxL.hack"},
+		{"./asm/max/Max.asm", "./asm/max/Max.hack", "./testdata/Max.hack"},
 		{"./asm/pong/PongL.asm", "./asm/pong/PongL.hack", "./testdata/PongL.hack"},
 		{"./asm/rect/RectL.asm", "./asm/rect/RectL.hack", "./testdata/RectL.hack"},
 		{"./asm/add/Add.asm", "./asm/add/Add.hack", "./testdata/Add.hack"},
