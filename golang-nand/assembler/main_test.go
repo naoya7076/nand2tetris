3afbd91c0fc426aef0ee64561fd0ffdc97876fc0
naoya7076 <naoya7076@gmail.com>
2024-02-27 09:03:03 +0900

    Rect.asmのテストを追加

--- golang-nand/assembler/main_test.go
+++ golang-nand/assembler/main_test.go
@@ -18,6 +18,7 @@ func TestAssemble(t *testing.T) {
 		{"./asm/pong/PongL.asm", "./asm/pong/PongL.hack", "./testdata/PongL.hack"},
 		{"./asm/pong/Pong.asm", "./asm/pong/Pong.hack", "./testdata/Pong.hack"},
 		{"./asm/rect/RectL.asm", "./asm/rect/RectL.hack", "./testdata/RectL.hack"},
+		{"./asm/rect/Rect.asm", "./asm/rect/Rect.hack", "./testdata/Rect.hack"},
 		{"./asm/add/Add.asm", "./asm/add/Add.hack", "./testdata/Add.hack"},
 	}
 	for _, tt := range cases {
